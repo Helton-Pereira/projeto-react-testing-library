d008dee0b5e80d2ba5e2bf040fa4ee361bf67e7f
Helton Pereira <helton.bfsm@gmail.com>
2022-08-16 18:13:50 -0300

    Requisito 3 cumprido(?)

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa a página Favorite Pokemons', () => {
+  test(`Verifica se a mensagem No favorite pokemon 
+  found aparece se não houver pokemons salvos`, () => {
+    render(<FavoritePokemons />);
+
+    const noFavoritePokemon = screen.queryByText(/No favorite pokemon found/i);
+    expect(noFavoritePokemon).toBeInTheDocument();
+  });
+  // test('Verifica se exibe todos os pokemons salvos', () => {
+  //   renderWithRouter(<App />);
+
+  //   const moreDetails = screen.getByRole('link', { name: /more details/i });
+  //   userEvent.click(moreDetails);
+  // });
+});
